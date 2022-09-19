73b481786ca9b20854533ba6fce5d36bedcc6007
waterbeen0530 <hange253@gmail.com>
2022-09-19 21:31:18 +0900

    [CREATE] Test:(modalPages)
    MondPotato, SweetChicken

---
+++ src/components/testPage/MondPtato.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export default function MondPotato({ view, food }) {
+  return (
+    view && (
+      <Container>
+        <Modal onClick={food}>
+          <p>
+            당신은 '몬드 감자전'보다 '달콤달콤 닭고기 스튜'를 더 좋아하는
+            사람입니다!
+          </p>
+        </Modal>
+      </Container>
+    )
+  );
+}
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #20202060;
+  text-align: center;
+`;
+
+const Modal = styled.div`
+  width: 600px;
+  height: 350px;
+  border-radius: 20px;
+  background-color: white;
+`;
